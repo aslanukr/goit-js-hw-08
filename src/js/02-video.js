7dadf62ae351d56efcb4cef9054d601d27fff913
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-03-13 18:06:07 +0200

    completes Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const SAVED_TIME = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    let playedTime = e.seconds;
+    localStorage.setItem(SAVED_TIME, playedTime);
+  }, 1000)
+);
+
+const localStoredPlaytime = localStorage.getItem(SAVED_TIME);
+
+if (localStoredPlaytime) {
+  player.setCurrentTime(localStoredPlaytime);
+}
