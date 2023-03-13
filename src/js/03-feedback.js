88e4331ffa064c6c5623f851fbf51f8dbea8451e
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-03-14 00:17:24 +0200

    completes Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const USER_FORM_KEY = 'feedback-form-state';
+
+let userFormData = {};
+
+const refs = {
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+populateUserForm();
+
+const userFormEl = document.querySelector('.feedback-form');
+userFormEl.addEventListener('submit', handleFormSubmit);
+userFormEl.addEventListener('input', throttle(handleDataInput, 500));
+
+function handleDataInput(e) {
+  userFormData[e.target.name] = e.target.value;
+  localStorage.setItem(USER_FORM_KEY, JSON.stringify(userFormData));
+}
+
+function populateUserForm() {
+  const savedFormData = localStorage.getItem(USER_FORM_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+
+  if (savedFormData) {
+    userFormData = parsedFormData;
+    refs.email.value = parsedFormData.email;
+    refs.message.value = parsedFormData.message;
+  }
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  if (refs.email.value !== '' && refs.message.value !== '') {
+    console.log(userFormData);
+    localStorage.removeItem(USER_FORM_KEY);
+    e.target.reset();
+    return;
+  }
+  alert(`All fields must be filled!`);
+}
